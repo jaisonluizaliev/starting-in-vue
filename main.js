7ec9f81b651d4a80f0046400cc6b2cb36889cf33
Jaison Alievi <jaisonalievi@vhintranet.com.br>
2022-12-27 09:36:25 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: 'An Another Dev Vue',
+      description: 'An Dev that learn vue to work in a new company',
+      date: new Date(),
+      author: 'unknown',
+    };
+  },
+});
